1aa59ee64af07bd9548acca852e4469cb6b8553f
Vicenzo-Az <vicenzo.net@gmail.com>
2025-04-08 15:28:55 -0300

    Add mais aulas

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,10 @@
-from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def blog(request):
     print('blog')
-    return HttpResponse('blog do app')
+    return render(request, 'blog/index.html')
 
 def exemplo(request):
     print('exemplo blog')
-    return HttpResponse('Exemplo de blog')
+    return render(request, 'blog/exemplo.html')
