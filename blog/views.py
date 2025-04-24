318ae83b23d75e49ae8986dc262e9391fcbb983c
Vicenzo-Az <vicenzo.net@gmail.com>
2025-04-24 17:30:56 -0300

    Add mais aulas

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-
+from typing import Any
 from blog.data import posts
+from django.http import HttpRequest, Http404
 
 def blog(request):
     print('blog')
 
     context = {
-        'text': 'Olá Blog',
+        # 'text': 'Olá Blog',
         'title': 'Blog',
         'posts': posts
     }
@@ -17,6 +18,28 @@ def blog(request):
         context,
     )
 
+def post(request: HttpRequest, post_id: int):
+    found_post: dict[str, Any] | None = None
+
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+
+    if found_post is None:
+        raise Http404('Post não existe.')
+
+    context = {
+        # 'text': 'Olá Blog',
+        'title': f'Post {found_post['id']}',
+        'post': found_post
+    }
+
+    return render(
+        request, 
+        'blog/post.html',
+        context,
+    )
 
 def exemplo(request):
     print('exemplo blog')
